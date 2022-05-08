99a83f7921a86a5988b2f37a11dee71a7ad07ff0
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-05-08 21:26:41 +0200

    Modify dropdown function and validate function - prevent against select the same worker in many inputs

--- script.js
+++ script.js
@@ -58,8 +58,10 @@ const workers = [
 
 const autocomplete = (input, autocompleteList, list) => {
     clearList();
+    if (list === workers) list = preventRepeat(list);
+
     list.forEach((item) => {
-        if (item.toLowerCase().startsWith(input.value.toLowerCase()) && input.value != "") {
+        if (item.toLowerCase().startsWith(input.value.toLowerCase()) && input.value !== "") {
             createList(input, autocompleteList, item);
         }
     })
@@ -67,7 +69,9 @@ const autocomplete = (input, autocompleteList, list) => {
 
 const dropdown = (input, autocompleteList, list) => {
     clearList();
-    list.forEach((item) => createList(input, autocompleteList, item))
+    if (list === workers) list = preventRepeat(list);
+
+    if (input.value === "") list.forEach((item) => createList(input, autocompleteList, item))    
 }
 
 function createList(input, autocompleteList, item) {
@@ -97,6 +101,21 @@ function clearList() {
     lists.forEach((list) => {
         list.style.display = "none"
     });
+}
+
+function preventRepeat(list) {
+    lessWorkers = list.map((worker) => {
+        if (workerInput1.value !== worker && 
+            workerInput2.value !== worker && 
+            workerInput3.value !== worker && 
+            workerInput4.value !== worker) 
+            {
+                return worker;
+            } else {
+                return null;
+        }
+    })
+    return lessWorkers;
 }
 
 
@@ -150,7 +169,7 @@ function validateForm() {
         return false;
     }
 
-    if (!cities.includes(document.form.cityInput2.value) && document.form.workerInput2.value != "") {
+    if (!cities.includes(document.form.cityInput2.value) && document.form.cityInput2.value !== "") {
         document.form.cityInput2.focus()
         alert(`${document.form.cityInput2.value} nie znajduje się w bazie miast.`);
         return false;
@@ -168,26 +187,36 @@ function validateForm() {
         return false;
     }
 
-    if (!workers.includes(document.form.workerInput3.value) && document.form.workerInput2.value != "") {
+    if (!workers.includes(document.form.workerInput2.value) && document.form.workerInput2.value !== "") {
+        document.form.workerInput2.focus()
+        alert(`${document.form.workerInput2.value} nie znajduje się na liście pracowników.`);
+        return false;
+    }
+
+    if (!workers.includes(document.form.workerInput3.value) && document.form.workerInput3.value !== "") {
         document.form.workerInput3.focus()
         alert(`${document.form.workerInput3.value} nie znajduje się na liście pracowników.`);
         return false;
     }
 
-    if (!workers.includes(document.form.workerInput3.value) && document.form.workerInput3.value != "") {
-        document.form.workerInput3.focus()
-        alert(`${document.form.workerInput3.value} nie znajduje się na liście pracowników.`);
-        return false;
-    }
-
-    if (!workers.includes(document.form.workerInput4.value) && document.form.workerInput4.value != "") {
+    if (!workers.includes(document.form.workerInput4.value) && document.form.workerInput4.value !== "") {
         document.form.workerInput4.focus()
         alert(`${document.form.workerInput4.value} nie znajduje się na liście pracowników.`);
         return false;
     }
 
+    let workerInputValues = [workerInput1.value, workerInput2.value, workerInput3.value, workerInput4.value];
+    let duplicateWorkers = workerInputValues
+                                .filter((item, index) => workerInputValues.indexOf(item) !== index)
+                                .filter(item => item != "");
+
+    if (duplicateWorkers.length !== 0) {
+        alert("Przynajmniej jeden z pracowników został wpisany więcej niż raz.")
+        return false;
+    }
+
     alert("Wysłano zamówienie");
-    
+
     return true;
 }
 
