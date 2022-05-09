5267cca5c01315924eef1bc79445eccc895c32d8
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-05-09 21:42:22 +0200

    Organize script.js file, add comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Input variables
 const cityInput1 = document.getElementById("cityInput1");
 const cityInput2 = document.getElementById("cityInput2");
 const cityInput3 = document.getElementById("cityInput3");
@@ -6,10 +7,8 @@ const workerInput2 = document.getElementById("workerInput2");
 const workerInput3 = document.getElementById("workerInput3");
 const workerInput4 = document.getElementById("workerInput4");
 
-const dateInput = document.getElementById("dateInput");
-const today = new Date();
-const plusTwoWeeks = new Date(Date.now() + 12096e5);
 
+// List variables
 const autocompleteList1 = document.getElementById("autocompleteList1");
 const autocompleteList2 = document.getElementById("autocompleteList2");
 const autocompleteList3 = document.getElementById("autocompleteList3");
@@ -18,6 +17,14 @@ const autocompleteList5 = document.getElementById("autocompleteList5");
 const autocompleteList6 = document.getElementById("autocompleteList6");
 const autocompleteList7 = document.getElementById("autocompleteList7");
 
+
+// Date variables to set the range of available dates
+const dateInput = document.getElementById("dateInput");
+const today = new Date();
+const plusTwoWeeks = new Date(Date.now() + 12096e5);
+
+
+// List of available cities
 const cities = [
     "Katowice",
     "Chorzów",
@@ -37,6 +44,8 @@ const cities = [
     "Czeladź",
 ];
 
+
+// List of available workers
 const workers = [
     "Ania",
     "Artur",
@@ -54,57 +63,76 @@ const workers = [
     "Weronika",
     "Wiktor",
     "Zygmunt",
-]
+];
 
-const autocomplete = (input, autocompleteList, list) => {
+
+// Autocomplete input field
+function autocomplete(input, autocompleteList, list) {
+    // Clear autocomplete list if is displayed
     clearList();
+
+    // Call preventRepeat function if list param is workers list
     if (list === workers) list = preventRepeat(list);
 
+    // Create autocomplete list only with words starts with input value
     list.forEach((item) => {
         if (item.toLowerCase().startsWith(input.value.toLowerCase()) && input.value !== "") {
             createList(input, autocompleteList, item);
-        }
-    })
-}
+        };
+    });
+};
 
-const dropdown = (input, autocompleteList, list) => {
+
+// Display dropdown list with all options
+function dropdown(input, autocompleteList, list) {
+    // Clear autocomplete list if is displayed
     clearList();
+
+    // Call preventRepeat function if list parameter is workers list
     if (list === workers) list = preventRepeat(list);
 
-    if (input.value === "") list.forEach((item) => createList(input, autocompleteList, item))    
-}
+    /// Create dropdown list with all options
+    if (input.value === "") list.forEach((item) => createList(input, autocompleteList, item))  ;  
+};
 
+
+// Create list items
 function createList(input, autocompleteList, item) {
+    // Set lists visibility
     autocompleteList.style.display = "block";
             
+    // Create list items and add all properties
     let autocompleteItem = document.createElement("li");
     autocompleteItem.classList.add("autocomplete-item");
     autocompleteItem.innerHTML = item;
     autocompleteList.appendChild(autocompleteItem);
-    autocompleteItem.addEventListener("click", () => {
-        fillInput(item, input);
-    })
-}
+    autocompleteItem.addEventListener("click", () => fillInput(item, input));
+};
 
+
+// Fill input field with passed value
 function fillInput(value, input) {
     input.value = value;
     clearList();
-}
+};
 
+
+// Remove list with all items
 function clearList() {
+    // Remove all items from list
     let items = document.querySelectorAll(".autocomplete-item");
-    items.forEach((item) => {
-        item.remove();
-    });
+    items.forEach((item) => item.remove());
 
+    // Change lists visibility
     let lists = document.querySelectorAll(".autocomplete-list");
-    lists.forEach((list) => {
-        list.style.display = "none"
-    });
-}
+    lists.forEach((list) => list.style.display = "none");
+};
 
+
+// Prevent from repeating values in many inputs - used only for workers
+// Return new list without values existing in other inputs
 function preventRepeat(list) {
-    lessWorkers = list.map((worker) => {
+    let lessWorkers = list.map((worker) => {
         if (workerInput1.value !== worker && 
             workerInput2.value !== worker && 
             workerInput3.value !== worker && 
@@ -113,12 +141,13 @@ function preventRepeat(list) {
                 return worker;
             } else {
                 return null;
-        }
-    })
+        };
+    });
     return lessWorkers;
-}
+};
 
 
+// Assing autocomplete and dropdown functions to input fields
 cityInput1.addEventListener("keyup", () => autocomplete(cityInput1, autocompleteList1, cities));
 cityInput1.addEventListener("click", () => dropdown(cityInput1, autocompleteList1, cities));
 
@@ -141,70 +170,78 @@ workerInput4.addEventListener("keyup", () => autocomplete(workerInput4, autocomp
 workerInput4.addEventListener("click", () => dropdown(workerInput4, autocompleteList7, workers));
 
 
-const formatDate = (date) => {
+// Transform date to format yyyy-mm-dd
+function formatDate(date) {
     let year = date.getFullYear().toString();
-    let month = date.getMonth()+1;
-    if (month < 10) {
-        month = "0" + month.toString();
-    }
-    let day = date.getDate();
-    if (day < 10) {
-        day = "0" + day.toString();
-    }
-    return year + "-" + month + "-" + day;
-}
 
+    let month = date.getMonth()+1;
+    if (month < 10) month = `0${month.toString()}`;
+
+    let day = date.getDate();
+    if (day < 10) day = `0${day.toString()}`;
+    
+    return `${year}-${month}-${day}`;
+};
+
+
+// Set the range of available dates
 const min = formatDate(today);
 const max = formatDate(plusTwoWeeks);
 
+
+// Add attributes to date input field
 dateInput.setAttribute("min", `${min}`);
 dateInput.setAttribute("max", `${max}`);
 dateInput.setAttribute("onfocus", "(this.type='date')");
 
 
+// Validate form
 function validateForm() {
+    // Check if city input values exist in cities list
     if (!cities.includes(document.form.cityInput1.value)) {
-        document.form.cityInput1.focus()
+        document.form.cityInput1.focus();
         alert(`${document.form.cityInput1.value} nie znajduje się w bazie miast.`);
         return false;
-    }
+    };
 
     if (!cities.includes(document.form.cityInput2.value) && document.form.cityInput2.value !== "") {
-        document.form.cityInput2.focus()
+        document.form.cityInput2.focus();
         alert(`${document.form.cityInput2.value} nie znajduje się w bazie miast.`);
         return false;
-    }
+    };
 
     if (!cities.includes(document.form.cityInput3.value)) {
-        document.form.cityInput3.focus()
+        document.form.cityInput3.focus();
         alert(`${document.form.cityInput3.value} nie znajduje się w bazie miast.`);
         return false;
-    }
+    };
 
+    // Check if worker input values exist in workers list
     if (!workers.includes(document.form.workerInput1.value)) {
-        document.form.workerInput1.focus()
+        document.form.workerInput1.focus();
         alert(`${document.form.workerInput1.value} nie znajduje się na liście pracowników.`);
         return false;
-    }
+    };
 
     if (!workers.includes(document.form.workerInput2.value) && document.form.workerInput2.value !== "") {
-        document.form.workerInput2.focus()
+        document.form.workerInput2.focus();
         alert(`${document.form.workerInput2.value} nie znajduje się na liście pracowników.`);
         return false;
-    }
+    };
 
     if (!workers.includes(document.form.workerInput3.value) && document.form.workerInput3.value !== "") {
-        document.form.workerInput3.focus()
+        document.form.workerInput3.focus();
         alert(`${document.form.workerInput3.value} nie znajduje się na liście pracowników.`);
         return false;
-    }
+    };
 
     if (!workers.includes(document.form.workerInput4.value) && document.form.workerInput4.value !== "") {
-        document.form.workerInput4.focus()
+        document.form.workerInput4.focus();
         alert(`${document.form.workerInput4.value} nie znajduje się na liście pracowników.`);
         return false;
-    }
+    };
 
+    // Check if all worker input values are different
     let workerInputValues = [workerInput1.value, workerInput2.value, workerInput3.value, workerInput4.value];
     let duplicateWorkers = workerInputValues
                                 .filter((item, index) => workerInputValues.indexOf(item) !== index)
@@ -218,12 +255,13 @@ function validateForm() {
     alert("Wysłano zamówienie");
 
     return true;
-}
+};
 
 
+// Clear form after click cancel button
 function clearForm() {
     let form = document.getElementsByName("form")[0];
     form.reset();
     dateInput.type="text";
     clearList();
-}
+};
